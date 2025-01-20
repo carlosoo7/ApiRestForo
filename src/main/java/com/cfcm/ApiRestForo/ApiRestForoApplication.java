d2d076971bde4c6842bc71f190e8bfc0ce5f2d59
Carlos Fernando Calderon Mora <151805986+carlosoo7@users.noreply.github.com>
2025-01-20 09:44:10 -0500

    Terminando la creacion inicial del proyecto

---
+++ src/main/java/com/cfcm/ApiRestForo/ApiRestForoApplication.java
@@ -0,0 +1,13 @@
+package com.cfcm.ApiRestForo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestForoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cfcm/ApiRestForo/ApiRestForoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfcm.ApiRestForo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
